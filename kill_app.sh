995f5c9682cb8590df19177e4b330cac10f7ba1a
Patchanonss <patchanonssc@gmail.com>
2025-12-14 13:10:33 +0700

    remove accident commited folder

--- kill_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "🔪 Killing process on Port 8000..."
-lsof -ti:8000 | xargs kill -9 2>/dev/null
-if [ $? -eq 0 ]; then
-    echo "✅ Port 8000 is now free."
-else
-    echo "👍 Port 8000 was already free."
-fi
